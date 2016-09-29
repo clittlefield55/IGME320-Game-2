4f56cd46115cd20a87bc551a2b7f4a5261390d8e
axw1698 <axw1698@g.rit.edu>
2016-09-29 15:17:48 -0400

    upload

---
+++ GDD2P2Dreamer/Assets/Scripts/Seeker.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections;
+
+public class Seeker : Vehicle {
+
+    public GameObject seekerTarget;
+
+
+    //weighting
+    public float seekWeight = 60.0f;
+
+    //ultimate steering force that will be applied to acceleration
+    private Vector3 ultimateForce;
+
+    //define necessary weights for seeker
+    public float safeDistance = 10.0f;
+    //public float avoidWeight = 30.0f;
+    //public float sepWeight = 10.0f;
+    //public float allignWeight = 20.0f;
+    //public float cohWeight = 50.0f;
+
+
+
+
+    // Call Inherited Start and then do our own
+    override public void Start() {
+        base.Start();
+        ultimateForce = Vector3.zero;
+    }
+
+    protected override void CalcSteeringForces() {
+        //reset ultimate force
+        ultimateForce = Vector3.zero;
+
+        //get a seeking force (based on char movement - for now, just seek)
+        //add that seeking force to the ultimate steering force
+
+        Vector3 dist = seekerTarget.transform.position - transform.position;
+        if (dist.magnitude <= 4) {
+            maxSpeed = 1f;
+            ultimateForce += Seek(seekerTarget.transform.position) * seekWeight;
+        } else {
+            ultimateForce += Seek(seekerTarget.transform.position - new Vector3(0, 0, 0)) * seekWeight;
+        }
+
+        //avoiding obstacle
+        //for(int i = 0; i < gm.Obstacles.Length; i++){
+        //	ultimateForce += AvoidObstacle(gm.Obstacles[i], 5) * avoidWeight;
+        //}
+
+        //limit steering force
+        ultimateForce = Vector3.ClampMagnitude(ultimateForce, maxForce);
+
+        //applyForce to acceleration
+        ApplyForce(ultimateForce);
+    }
+
+}
---
+++ GDD2P2Dreamer/Assets/Scripts/Vehicle.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using System.Collections;
+
+//use the Generic system here to make use of a Flocker list later on
+using System.Collections.Generic;
+
+[RequireComponent(typeof(CharacterController))]
+
+abstract public class Vehicle : MonoBehaviour {
+
+	//no position - transform.position will be used instead
+	protected Vector3 acceleration;
+	protected Vector3 velocity;
+	protected Vector3 desired;
+
+	//access to game manager script
+	//protected GameManager gm;
+
+	public Vector3 Velocity {
+		get { return velocity; }
+	}
+	
+	public float maxSpeed = 3.0f;
+	public float maxForce = 6.0f;
+	public float mass = 1.0f;
+	public float radius = 1.0f;
+
+	CharacterController charControl;
+
+	abstract protected void CalcSteeringForces();
+
+	virtual public void Start(){
+		acceleration = Vector3.zero;
+		velocity = transform.forward;
+		desired = Vector3.zero;
+
+		//store access to character controller component
+		charControl = GetComponent<CharacterController>();
+
+		//access to game obj holding gm script
+		//gm = GameObject.Find("GameManagerGO").GetComponent<GameManager>();
+	}
+
+	
+	// Update is called once per frame
+	protected void Update () {
+		CalcSteeringForces ();
+
+		//add accel to vel
+		velocity += acceleration * Time.deltaTime;
+		velocity.y = 0;
+		//limit velo to max speed
+		velocity = Vector3.ClampMagnitude (velocity, maxSpeed);
+		//dude face torward target
+		transform.forward = velocity.normalized;
+		//added vel to pos to move
+		charControl.Move (velocity * Time.deltaTime);
+
+		Vector3 pos = transform.position;
+		transform.position = pos;
+		//reset accel
+		acceleration = Vector3.zero;
+
+	}
+
+	protected void ApplyForce(Vector3 steeringForce){
+		acceleration += steeringForce / mass;
+	}
+
+	protected Vector3 Seek(Vector3 targetPosition){ 
+		desired = targetPosition - transform.position;
+		desired = desired.normalized * maxSpeed;
+		Vector3 seekingForce = desired - velocity;
+		seekingForce.y = 0; 
+		return seekingForce; 
+	}
+
+
+	/*protected Vector3 AvoidObstacle(GameObject obst, float sD){
+		desired = Vector3.zero;
+		//distance from dude to obstacle
+		Vector3 vecToCenter = obst.transform.position - transform.position;
+		vecToCenter.y =0;
+		//radius of obstacle
+		float obstRad = obst.GetComponent<ObstacleScript> ().Radius;
+		//calculate safe distance
+		if (vecToCenter.magnitude > sD) {
+			return Vector3.zero;
+		}
+		//check obstacle behind
+		if (Vector3.Dot (vecToCenter, transform.forward) < 0) {
+			return Vector3.zero;
+		}
+		//will it not collide?
+		if (Mathf.Abs (Vector3.Dot (vecToCenter, transform.right)) > obstRad + radius) {
+			return Vector3.zero;
+		}
+		//will it collide?
+		//is it on your left or right?
+		if(Vector3.Dot(vecToCenter, transform.right) < 0){ //on left move right
+			desired = transform.right * maxSpeed;
+			//debug line to see if the dude is avoiding to the right
+			//Debug.DrawLine(transform.position, obst.transform.position, Color.red);
+			Debug.DrawLine(obst.transform.position, transform.position, Color.red);
+		}
+		else { //on right move left
+			desired = transform.right * -maxSpeed;
+			//debug line to see if the dude is avoiding to the left
+			Debug.DrawLine(transform.position, obst.transform.position, Color.green);
+		}
+		return desired;
+	}*/
+
+
+}
