8a3e1dd7cf0bf80b76bafcde1a9f1b017fc2f0f7
Ellen <emc7541@ad.rit.edu>
2016-10-03 15:39:31 -0400

    Added collectible script
    Add the script (not exactly working) to collect items

---
+++ GDD2P2Dreamer/Assets/Scripts/Collectible.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectible : MonoBehaviour {
+
+    public int itemsCollected = 0;
+
+    void OnCollisionEnter(UnityEngine.Collision col)
+    {
+        if (col.gameObject.tag == "Collectible")
+        {
+            print("Tick");  // debug
+            Destroy(col.gameObject);
+            itemsCollected++;
+        }
+    }
+}
