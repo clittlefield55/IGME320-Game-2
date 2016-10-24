b93a395889ef44b22049f74e25dacb3b0e7706a7
axw1698 <axw1698@g.rit.edu>
2016-10-24 16:58:59 -0400

    update menu

--- GDD2P2Dreamer/Assets/Scripts/UI/UIControl.cs
+++ GDD2P2Dreamer/Assets/Scripts/UI/UIControl.cs
@@ -2,9 +2,32 @@
 using System.Collections;
 
 public class UIControl : MonoBehaviour {
-
+    GameObject main;
+    GameObject page1;
+    GameObject page2;
+    void Start()
+    {
+        main = GameObject.Find("Main");
+        page1 = GameObject.Find("Page1");
+        page2 = GameObject.Find("Page2");
+        page1.SetActive(false);
+        page2.SetActive(false);
+    }
     public void ChangeScene(string sceneName)
     {
         Application.LoadLevel(sceneName);
+    }
+    public void ChangePage(int id)
+    {
+        if(id == 0) // change to page 1
+        {
+            main.SetActive(false);
+            page1.SetActive(true);
+        }
+        else if(id == 1) // change to page 2
+        {
+            page1.SetActive(false);
+            page2.SetActive(true);
+        }
     }
 }
